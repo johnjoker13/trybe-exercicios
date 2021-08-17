084a12547a328351a0165d1b7be208cec8d8556c
joaoOliveira <joaoavelino2008@hotmail.com>
2021-08-17 10:47:32 -0300

    add exercicio dia02 bloco02

---
+++ fundamentos/bloco-02-git-e-github-e-internet/dia-02-git-github-entendendo-os-comandos/exercicio-entendendo-os-comandos.sh
@@ -0,0 +1,37 @@
+#1
+trybe-exercicios/fundamentos/bloco-02-git-e-github-e-internet/dia-02-git-github-entendendo-os-comandos
+
+#2
+git status
+
+#3
+git checkout -b trybe-skills-change
+
+#4
+cat >> trybe_skills.txt
+- HTML
+- CSS
+git add trybe_skills.txt
+git commit -m "Adiciona novas skills"
+git push -u origin trybe-skills-change
+
+#5
+#entrar no GitHub e realizar o Pull Request
+
+#6
+git checkout main
+
+#7
+git branch
+
+#8
+git checkout -b branch trybe-skills-updates
+
+#9 
+cat >> trybe_skills.txt
+git add trybe_skills.txt
+git commit -m "Adiciona nova skill"
+git push -u origin trybe-skills-updates
+
+
+#10 O merge deve ser realizado dentro do Github.
