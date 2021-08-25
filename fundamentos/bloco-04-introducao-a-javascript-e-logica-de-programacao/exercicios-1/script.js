f17edd815921060a9110e94791dadfb55637e9cb
joaoOliveira <57510427+johnjoker13@users.noreply.github.com>
2021-08-25 15:59:01 -0300

    Delete fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-1 directory

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-1/script.js
+++
@@ -1,8 +0,0 @@
-const myName = "Joao";
-const birthCity = "Sao Paulo";
-var birthYear = 1999;
-console.log(myName + " " + birthCity + " " + birthYear);
-birthYear = 2030;
-console.log(birthYear);
-birthCity = "Minas Gerais"
-console.log(birthCity);
