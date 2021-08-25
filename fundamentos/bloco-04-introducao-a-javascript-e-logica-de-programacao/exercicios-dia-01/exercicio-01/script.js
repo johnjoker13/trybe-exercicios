a87770dac0c618e7e0096683c0db5431bbe6fbd2
joaoOliveira <joaoavelino2008@hotmail.com>
2021-08-25 18:22:50 -0300

    corrigindo path

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-dia-01/exercicio-01/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-dia-01/exercicio-01/script.js
@@ -1,6 +1,6 @@
 const myName = "Joao";
 const birthCity = "Sao Paulo";
-var birthYear = 1999;
+let birthYear = 1999;
 console.log(myName + " " + birthCity + " " + birthYear);
 birthYear = 2030;
 console.log(birthYear);
