87d746a22fd13599ff6cb8520e73da16e5b57cca
joaoOliveira <joaoavelino2008@hotmail.com>
2021-08-25 16:50:10 -0300

    exercicio-04 dia-01

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-dia-01/exercicio-04/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-dia-01/exercicio-04/script.js
@@ -42,3 +42,42 @@ if(currentHour==11 || currentHour<=14){
 if(currentHour==4 || currentHour<=11){
     console.log(message = "Hmmm, cheiro de café recém passado");
 }
+
+const bebidaPrincipal =  'cafezinho';
+const bebidaAlternativa = 'suco de laranja';
+
+if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+  console.log("Obrigado por me atender :D")
+} else {
+  console.log("Ei, eu não pedi isso!");
+}
+
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+let weekDay = "sábado";
+if(weekDay=="segunda-feira" || weekDay=="terça-feira" || weekDay=="quarta-feira" || weekDay=="quinta-feira" || weekDay=="sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else 
+    if(weekDay =="sábado" || weekDay == "domingo"){
+    console.log("FINALMENTE, descando merecido UwU");
+}
+
+console.log((2 + 2) === 4);
+
+console.log(!(2 + 2) === 4);
+
+const squirtle = "melhor pokemon inicial";
+
+console.log(!squirtle); // false
+
+console.log(!42); // false
+
+console.log(!0); // true
+// O número 0 tem o valor "falsy" no javascript. Logo, seu oposto é true.
+
+console.log(!null); // true
+
+console.log(!undefined); // true
