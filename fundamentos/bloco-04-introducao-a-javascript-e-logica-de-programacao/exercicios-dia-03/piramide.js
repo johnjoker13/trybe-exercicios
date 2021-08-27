ea6c04b424dbf582b3eac8ba721190854774cb1d
joaoOliveira <joaoavelino2008@hotmail.com>
2021-08-27 17:46:51 -0300

    exercicios dia-03

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-dia-03/piramide.js
@@ -0,0 +1,15 @@
+function reserveTriangle(tamanho) {
+  for (let i = 0; i < tamanho; i++) {
+    let aux = " ";
+    for (let j = 0; j < tamanho; j++) {
+      if (j >= tamanho - i - 1) {
+        aux += " *";
+      } else {
+        aux += " ";
+      }
+    }
+    console.log(aux);
+  }
+}
+
+reserveTriangle(5);
