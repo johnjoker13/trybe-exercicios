89484db22b60fbea817934e2939a36f15781a47b
joaoOliveira <joaoavelino2008@hotmail.com>
2021-08-30 17:25:46 -0300

    exercicios dia-04

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-dia-04/exercicio-12.js
@@ -0,0 +1,15 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+
+    let infoDois ={
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  };
+
+  console.log(info.personagem + " " + "e" + " " + infoDois.personagem + "\n " + info.origem + " " + "e" + " " + infoDois.origem + "\n" + info.nota + " " + "e" + " " + infoDois.nota + "\n" + "Ambos recorrentes");
