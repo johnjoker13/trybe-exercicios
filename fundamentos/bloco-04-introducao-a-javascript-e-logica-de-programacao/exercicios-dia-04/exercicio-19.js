cd44e3e1b97667db419f03ced2317ac33a486641
joaoOliveira <joaoavelino2008@hotmail.com>
2021-09-01 22:00:10 -0300

    exercicio-19 dia-04

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-dia-04/exercicio-19.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-dia-04/exercicio-19.js
@@ -1,3 +1,20 @@
-function maisRepetido(){
-    
+let arrayTeste = [2, 3, 2, 5, 8, 2, 3];
+
+function maisRepetido(arr) {
+  let occour = 0;
+  for (let index of arr) {
+    let result = 0;
+    for (let n of arr) {
+      if (index === n) {
+        result += 1;
+      }
+    }
+    if (result > occour) {
+      occour = result;
+    }
+  }
+  return console.log(occour);
 }
+
+maisRepetido(arrayTeste);
+
