880ac8866201bf2260bdb6674e4f6311b106dd49
joaoOliveira <joaoavelino2008@hotmail.com>
2021-10-06 15:03:53 -0300

    exercicio 8

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-fixacao/ex08.js
@@ -0,0 +1,11 @@
+const people = [
+  { name: "Mateus", age: 18 },
+  { name: "José", age: 16 },
+  { name: "Ana", age: 23 },
+  { name: "Cláudia", age: 20 },
+  { name: "Bruna", age: 19 },
+];
+
+// Adicione se código aqui
+
+console.log(Object.values(people).sort((x, y) => y.age - x.age));
