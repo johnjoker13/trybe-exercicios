cc217abfbf2c1957efc243d609a4f3d69c5fd754
joaoOliveira <joaoavelino2008@hotmail.com>
2021-10-27 15:31:21 -0300

    exercicio-fixacao-1 10.2

---
+++ fundamentos/bloco-10-introducao-a-react/dia-2-componentes-react/exercicios-fixacao/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "./Image";
+
+function App() {
+  return (
+    <div>
+      <Image
+        src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
+        alt="Cute cat staring"
+      />
+    </div>
+  );
+}
+
+export default App;
