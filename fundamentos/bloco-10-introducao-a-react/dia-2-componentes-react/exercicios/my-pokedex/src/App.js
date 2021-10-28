3655717f46c4bd17cdc7dbf95b5222e222995cba
joaoOliveira <joaoavelino2008@hotmail.com>
2021-10-28 18:51:06 -0300

    exercicios 10.2

---
+++ fundamentos/bloco-10-introducao-a-react/dia-2-componentes-react/exercicios/my-pokedex/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import pokemons from "./data";
+import Pokedex from "./Pokedex";
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Pokemon</h1>
+        <Pokedex pokemons={pokemons} />
+      </div>
+    );
+  }
+}
+
+export default App;
