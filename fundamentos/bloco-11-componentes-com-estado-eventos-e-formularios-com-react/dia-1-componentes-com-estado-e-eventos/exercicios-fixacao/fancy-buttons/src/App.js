946649eca8d9b36c65cc9334bf032dda48bdfc0a
joaoOliveira <joaoavelino2008@hotmail.com>
2021-11-03 18:50:12 -0300

    fixacao: exercicio 3

--- fundamentos/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-fixacao/fancy-buttons/src/App.js
+++ fundamentos/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-fixacao/fancy-buttons/src/App.js
@@ -8,24 +8,44 @@ class App extends React.Component {
     this.handleClickOne = this.handleClickOne.bind(this);
     this.handleClickTwo = this.handleClickTwo.bind(this);
     this.handleClickThree = this.handleClickThree.bind(this);
+
+    this.state = {
+      numeroDeCliques: 0,
+      clickNumber: 0,
+      numberClick: 0,
+    };
   }
 
-  handleClickOne() {
+  handleClickOne = () => {
     console.log("clickou:", this);
-  }
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1,
+    }));
+  };
+
   handleClickTwo() {
     console.log("clicked:", this);
+    this.setState((estadoAnterior, _props) => ({
+      clickNumber: estadoAnterior.clickNumber + 1,
+    }));
   }
   handleClickThree() {
     console.log("clickeou:", this);
+    this.setState((estadoAnterior, _props) => ({
+      numberClick: estadoAnterior.numberClick + 1,
+    }));
   }
 
   render() {
     return (
       <>
-        <button onClick={this.handleClickOne}>Click-Me</button>
-        <button onClick={this.handleClickTwo}>Click-Me</button>
-        <button onClick={this.handleClickThree}>Click-Me</button>
+        <button onClick={this.handleClickOne}>
+          {this.state.numeroDeCliques}
+        </button>
+        <button onClick={this.handleClickTwo}>{this.state.clickNumber}</button>
+        <button onClick={this.handleClickThree}>
+          {this.state.numberClick}
+        </button>
       </>
     );
   }
