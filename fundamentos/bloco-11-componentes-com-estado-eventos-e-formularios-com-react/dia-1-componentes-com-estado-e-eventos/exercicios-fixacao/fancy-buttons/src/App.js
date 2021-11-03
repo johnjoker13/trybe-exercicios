b68bb37db678a479d04e77505efb277f7b316b96
joaoOliveira <joaoavelino2008@hotmail.com>
2021-11-03 16:09:10 -0300

    fixacao: exercicio2

--- fundamentos/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-fixacao/fancy-buttons/src/App.js
+++ fundamentos/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-fixacao/fancy-buttons/src/App.js
@@ -2,12 +2,30 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.handleClickOne = this.handleClickOne.bind(this);
+    this.handleClickTwo = this.handleClickTwo.bind(this);
+    this.handleClickThree = this.handleClickThree.bind(this);
+  }
+
+  handleClickOne() {
+    console.log("clickou:", this);
+  }
+  handleClickTwo() {
+    console.log("clicked:", this);
+  }
+  handleClickThree() {
+    console.log("clickeou:", this);
+  }
+
   render() {
     return (
       <>
-        <button onClick={() => console.log("Clicado")}>Click-Me</button>
-        <button onClick={() => console.log("Clicked")}>Click-Me</button>
-        <button onClick={() => console.log("Clickeado")}>Click-Me</button>
+        <button onClick={this.handleClickOne}>Click-Me</button>
+        <button onClick={this.handleClickTwo}>Click-Me</button>
+        <button onClick={this.handleClickThree}>Click-Me</button>
       </>
     );
   }
