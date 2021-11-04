05d720938751cab476e2fe196ac31fa97d856c92
joaoOliveira <joaoavelino2008@hotmail.com>
2021-11-04 07:08:19 -0300

    refatorando exercicio

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-Javascript-Objetos-e-Funcoes/exercicios-dia-04/bonus.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-Javascript-Objetos-e-Funcoes/exercicios-dia-04/bonus.js
@@ -1,17 +1,17 @@
 const roman = {
-  1: "I",
-  4: "IV",
-  5: "V",
-  9: "IX",
-  10: "X",
-  40: "XL",
-  50: "L",
-  90: "XC",
-  100: "C",
-  400: "CD",
-  500: "D",
-  900: "CM",
-  1000: "M",
+  I: 1,
+  IV: 4,
+  V: 5,
+  IX: 9,
+  X: 10,
+  XL: 40,
+  L: 50,
+  XC: 90,
+  C: 100,
+  CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000,
 };
 
 const sumArray = (prev, next) => {
@@ -22,66 +22,18 @@ const sumArray = (prev, next) => {
   }
 };
 
-const returnNum = (str) => {
-  const newArr = [];
-  let translate = 0;
-  const splitStr = str.split("");
-
-  for (let i = 0; i < splitStr.length; i += 1) {
-    if (splitStr[i] === "I") {
-      translate = 1;
-      newArr.push(translate);
+const transformRoman = (alg) => {
+  const splitAlg = alg.split("");
+  const newArray = [];
+  const algValues = Object.keys(roman);
+  splitAlg.forEach((element) => {
+    if (algValues.includes(element)) {
+      newArray.push(roman[element]);
+    } else {
+      throw new Error(`Algarismo não reconhecido ${element}`);
     }
-    if (splitStr[i] === "IV") {
-      translate = 4;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "V") {
-      translate = 5;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "IX") {
-      translate = 9;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "X") {
-      translate = 10;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "XL") {
-      translate = 40;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "L") {
-      translate = 50;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "XC") {
-      translate = 90;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "C") {
-      translate = 100;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "CD") {
-      translate = 400;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "D") {
-      translate = 500;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "CM") {
-      translate = 900;
-      newArr.push(translate);
-    }
-    if (splitStr[i] === "M") {
-      translate = 1000;
-      newArr.push(translate);
-    }
-  }
-  return newArr.reduce(sumArray);
+  });
+  console.log(newArray.reduce(sumArray));
 };
 
-console.log(returnNum("MMXXI"));
+transformRoman("MMXXk");
