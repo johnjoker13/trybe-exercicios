1b89f717bee755c49f352d3e1826376c672dcf8c
joaoOliveira <joaoavelino2008@hotmail.com>
2022-01-24 19:02:24 -0300

    iniciando exercicios

---
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/exercicios-fixacao/src/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { cleanup } from '@testing-library/react';
+import renderWithRedux from './renderWithRedux';
+import App from './App';
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  it('the page should has a button and a text 0', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const buttonAdicionar = queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('0')).toBeInTheDocument();
+  });
+
+  it('a click in a button should increment the value of clicks', () => {
+    const { queryByText } = renderWithRedux(<App />, 
+      { initialState: { clickReducer: { counter: 5 }}});
+
+    expect(queryByText('5')).toBeInTheDocument();
+  });
+
+});
