d832d46fc4a7e1686a288b0633bc19d50ea2340e
joaoOliveira <joaoavelino2008@hotmail.com>
2022-02-22 15:32:55 -0300

    exercicios-4

---
+++ back-end/bloco-20-introducao-a-SQL/dia-2-encontrando-dados-em-um-banco-de-dados/exercicios-fixacao-dia-2/exercicio-4.2.sql
@@ -0,0 +1,14 @@
+USE Escola;
+SELECT DISTINCT Nome, Idade From Alunos;
+SELECT DISTINCT Nome FROM Alunos;
+SELECT DISTINCT Idade FROM Alunos;
+
+-- Respostas:
+-- P: Quantas linhas você encontraria na query anterior?
+-- R: 5
+
+-- P: Quantas linhas você encontraria na query anterior?
+-- R: 4
+
+-- P: Quantas linhas você encontraria na query anterior?
+-- R: 4;
