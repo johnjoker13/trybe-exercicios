fe43fe4a0c5a28214e75a7b4348b2008e68e9ee5
joaoOliveira <joaoavelino2008@hotmail.com>
2022-02-22 14:57:42 -0300

    exercicio-fixacao-1

---
+++ back-end/bloco-20-introducao-a-SQL/dia-2-encontrando-dados-em-um-banco-de-dados/exercicios-fixacao-dia-2/exercicio-1.sql
@@ -0,0 +1,4 @@
+SELECT 'João' AS nome, 'Oliveira' AS sobrenome, 23 AS idade, 'Desenvolvedor Web' AS 'Área de atuação';
+SELECT 'João' AS nome, 'Oliveira' AS sobrenome, 23 AS idade, 'São Paulo' AS 'Cidade Natal';
+SELECT 13*8 AS 'Resultado';
+SELECT now() AS 'Data Atual';
