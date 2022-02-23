6b96b53409652e0e9df0542d08b8166656aa7982
joaoOliveira <joaoavelino2008@hotmail.com>
2022-02-23 17:26:25 -0300

    exercicios-fixacao-4

--- back-end/bloco-19-docker/dia-3-filtrando-dados-de-forma-especifica/exercicios-fixacao/exercicio-3.sql
+++ back-end/bloco-19-docker/dia-3-filtrando-dados-de-forma-especifica/exercicios-fixacao/exercicio-3.sql
@@ -1,3 +1,5 @@
+USE sakila;
+
 SELECT * FROM customer WHERE last_name
 IN('hicks', 'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy')
 ORDER BY first_name ASC;
---
+++ back-end/bloco-19-docker/dia-3-filtrando-dados-de-forma-especifica/exercicios-fixacao/exercicio-4.sql
@@ -0,0 +1,31 @@
+USE sakila;
+
+SELECT DATE('2005-05-25') FROM payment;
+
+SELECT 
+    COUNT(*)
+FROM
+    payment
+WHERE
+    payment_date BETWEEN '2005-07-01' AND '2005-08-22';
+
+    SELECT 
+    DATE(rental_id),
+    YEAR(rental_id),
+    MONTH(rental_id),
+    DAY(rental_id),
+    MINUTE(rental_id),
+    SECOND(rental_id)
+FROM
+    rental
+WHERE
+    rental_id = 10330;
+
+
+SELECT 
+    *
+FROM
+    payment
+WHERE
+    DATE(payment_date) = '2005-07-28'
+        AND HOUR(payment_date) >= 22;
