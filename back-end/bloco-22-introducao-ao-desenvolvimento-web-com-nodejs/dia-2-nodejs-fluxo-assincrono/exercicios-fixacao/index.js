c7a9b403cde27b41e5474f8e9e290da4f1e25a95
joaoOliveira <joaoavelino2008@hotmail.com>
2022-03-14 16:41:02 -0300

    exercicio-fixacao-3

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios-fixacao/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios-fixacao/index.js
@@ -2,9 +2,16 @@ const firstFunc = require("./exercicio-1");
 const secondFunc = require("./exercicio-2");
 
 firstFunc(10, 40, 1)
-  .then((result) => console.log(`Primeiro: ${result}`))
+  .then((result) => console.log(result))
   .catch((err) => console.log(`Error: ${err}`));
 
-firstFunc(secondFunc(), secondFunc(), secondFunc())
-  .then((result) => console.log(`Segundo: ${result}`))
-  .catch((err) => console.log(`Error: ${err}`));
+const resultSecond = async () => {
+  try {
+    const result = await firstFunc(secondFunc(), secondFunc(), secondFunc());
+    console.log(result);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+  }
+};
+
+resultSecond();
