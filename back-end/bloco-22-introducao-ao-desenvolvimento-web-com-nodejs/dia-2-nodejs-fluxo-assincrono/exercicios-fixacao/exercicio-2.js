8f8ecaec81c301466ac77fb68741ea047937b2f3
joaoOliveira <joaoavelino2008@hotmail.com>
2022-03-14 16:36:11 -0300

    exercicio-fixacao-2

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios-fixacao/exercicio-2.js
@@ -0,0 +1,5 @@
+const secondFunc = () => {
+  return Math.floor(Math.random() * 100 + 1);
+};
+
+module.exports = secondFunc;
--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios-fixacao/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios-fixacao/index.js
@@ -1,5 +1,10 @@
 const firstFunc = require("./exercicio-1");
+const secondFunc = require("./exercicio-2");
 
 firstFunc(10, 40, 1)
-  .then((result) => console.log(result))
+  .then((result) => console.log(`Primeiro: ${result}`))
+  .catch((err) => console.log(`Error: ${err}`));
+
+firstFunc(secondFunc(), secondFunc(), secondFunc())
+  .then((result) => console.log(`Segundo: ${result}`))
   .catch((err) => console.log(`Error: ${err}`));
