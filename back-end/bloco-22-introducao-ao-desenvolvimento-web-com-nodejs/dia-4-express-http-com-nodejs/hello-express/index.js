440a1058726294a4ba02284808345f3398979868
joaoOliveira <joaoavelino2008@hotmail.com>
2022-03-16 22:16:59 -0300

    done

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-express/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-express/index.js
@@ -45,6 +45,37 @@ app.get("/recipes/:id", function (req, res) {
   res.status(200).json(recipe);
 });
 
+// ...
+
+app.put("/recipes/:id", function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1)
+    return res.status(404).json({ message: "Recipe not found!" });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+});
+
+//...
+
+app.delete("/recipes/:id", function (req, res) {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1)
+    return res.status(404).json({ message: "Recipe not found!" });
+
+  recipes.splice(recipeIndex, 1);
+
+  res.status(204).end();
+});
+
+//...
+
 const drinks = [
   { id: 1, name: "Refrigerante Lata", price: 5.0 },
   { id: 2, name: "Refrigerante 600ml", price: 8.0 },
@@ -62,7 +93,7 @@ app.get("/drinks", function (req, res) {
 app.post("/drinks", function (req, res) {
   const { id, name, price } = req.body;
   recipes.push({ id, name, price });
-  res.status(201).json({ message: "Recipe created successfully!" });
+  res.status(201).json({ message: "Drink created successfully!" });
 });
 
 app.get("/drinks/search", function (req, res) {
@@ -86,6 +117,11 @@ app.get("/validateToken", function (req, res) {
     return res.status(401).json({ message: "Invalid Token!" });
 
   res.status(200).json({ message: "Valid Token!" });
+});
+
+//...
+app.all("*", function (req, res) {
+  return res.status(404).json({ message: `Rota '${req.path}' não existe!` });
 });
 
 app.listen(3001, () => {
