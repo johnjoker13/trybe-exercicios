a36e43a84faeba185fbca9af206eb5d63d08da5e
joaoOliveira <joaoavelino2008@hotmail.com>
2022-03-28 20:27:39 -0300

    enviando requests com node-fetch

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/hello-http-methods/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/hello-http-methods/index.js
@@ -2,8 +2,31 @@
 
 const fetch = require('node-fetch');
 
+// Armazenamos o token numa variável.
+// Num ambiente real, esse valor viria do Local Storage, ou de uma variável de ambiente
+const API_TOKEN = '2d635ea9b637ea0f27d58985cc161d64';
+
+// Criamos um novo objeto de Headers
+const headers = new fetch.Headers({
+  Authorization: API_TOKEN,
+  'Content-Type': 'application/json',
+});
+
+// Depois, criamos o body
+// Utilizamos o `stringify` para que possamos enviar esse body como JSON
+
+const body = JSON.stringify({
+  name: 'Tryber',
+  email: 'tryber@betrybe.com',
+  password: 'Tr1b3r',
+});
+
 // Para aquecer, vamos começar com uma requisição do tipo `GET`
-fetch('https://postman-echo.com/get?param1=teste')
+fetch('https://postman-echo.com/post?param1=teste', {
+  headers,
+  method: 'POST',
+  body,
+})
   .then((response) => {
     // Ao receber a resposta, verificamos se correu tudo bem
     if (!response.ok) {
